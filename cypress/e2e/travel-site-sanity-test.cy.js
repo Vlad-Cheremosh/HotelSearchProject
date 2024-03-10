5727eef0f2b7298251d88c40880940d5b2a133ba
Vlad-Cheremosh <nomad.od.ua@gmail.com>
2024-03-10 19:21:38 -0400

    Completed

--- cypress/e2e/travel-site-sanity-test.cy.js
+++ cypress/e2e/travel-site-sanity-test.cy.js
@@ -27,7 +27,8 @@ describe('The sanity test suite for travel site', () => {
     cy.get('[data-cy="flight_chk"]').should("be.visible")
     cy.get('[data-cy="cars_chk"]').should("be.visible")
     // cy.get('#tab1 > form > .btn-submit').should("be.visible")
-    // cy.contains('search').should("be.visible")
+    
+    cy.contains('search').should("be.visible")
     cy.get('[type="submit"]').should("be.visible")
 
   })
